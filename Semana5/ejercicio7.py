3c0454f0510c4e0e9f5a0334738cb4623fedf31e
CARLOS <herramientas.carlos@mf.me>
2026-04-01 16:00:51 +0000

    ejercicio7

--- Semana5/ejercicio7.py
+++ Semana5/ejercicio7.py
@@ -9,16 +9,26 @@ Debe implementar:
 1. Una solución con iteración
 2. Una solución con recursividad
 """
+#ahorros = [100000, 200000, 37000, 30000]
 
 def ahorro_total_ciclo(ahorros):
-    """
-    Retorna el ahorro total usando iteración.
-    """
-    pass
+ largo = len(ahorros)
+ suma = 0
+ while (largo > 0):
+   suma = suma + ahorros[largo-1]
+   largo = largo -1
+ return suma
+
+#print(ahorro_total_ciclo(ahorros))  
+   
+
+
 
 
 def ahorro_total_recursivo(ahorros):
-    """
-    Retorna el ahorro total usando recursividad.
-    """
-    pass
+  largo = len(ahorros)
+  if (largo == 0):
+     return 0
+  else: return (ahorros[-1]+ ahorro_total_recursivo(ahorros[:-1])) 
+
+#print(ahorro_total_recursivo(ahorros))
