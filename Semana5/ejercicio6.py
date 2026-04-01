7046a2e9db67c8a32d6fc2d362e426f8f3f03cf3
CARLOS <herramientas.carlos@mf.me>
2026-04-01 16:06:54 +0000

    ejercicio6

--- Semana5/ejercicio6.py
+++ Semana5/ejercicio6.py
@@ -7,18 +7,24 @@ Debe implementar dos funciones:
 1. Una usando iteración (for o while)
 2. Una usando recursividad
 """
-
+ventas = [40, 50 , 60]
 def total_ventas_ciclo(ventas):
-    """
-    Retorna el total de ventas usando ciclos.
-    """
-    # Escriba aquí su solución
-    pass
+ largo = len(ventas)
+ suma = 0
+ while (largo > 0):
+   suma = suma + ventas[largo-1]
+   largo = largo -1
+ return suma
+
 
 
 def total_ventas_recursivo(ventas):
-    """
-    Retorna el total de ventas usando recursividad.
-    """
-    # Escriba aquí su solución
-    pass
+  largo = len(ventas)
+  if (largo == 0):
+     return 0
+  else: return (ventas[-1]+ total_ventas_recursivo(ventas[:-1])) 
+
+
+print(total_ventas_recursivo(ventas))
+print(total_ventas_ciclo(ventas))
+
